38ce0221dc5f15be7d2fc38f97b51f76e00cf9df
RenanCostaNunes <renandacostanunes@gmail.com>
2025-09-17 21:27:10 -0300

    Commit Final

--- tests/test_validador.py
+++ tests/test_validador.py
@@ -4,9 +4,9 @@ from validador import validar_cpf
 class TestValidadorCPF(unittest.TestCase):
 
     def test_deve_retornar_true_para_cpf_valido(self):
-        cpf_com_mascara = "529.982.247-25" #
-        cpf_sem_mascara = "52998224725" #
-        cpf_com_espacos = " 529.982.247-25 " #
+        cpf_com_mascara = "529.982.247-25" 
+        cpf_sem_mascara = "52998224725" 
+        cpf_com_espacos = " 529.982.247-25 "
 
         self.assertTrue(validar_cpf(cpf_com_mascara), "Deveria ser válido com máscara")
         self.assertTrue(validar_cpf(cpf_sem_mascara), "Deveria ser válido sem máscara")
