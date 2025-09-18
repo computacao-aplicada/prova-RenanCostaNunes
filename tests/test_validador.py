565ec92d42c9d72026ea748a4c0e639721225e70
RenanCostaNunes <renandacostanunes@gmail.com>
2025-09-17 21:46:02 -0300

    Toquezinho final, só detalhe

--- tests/test_validador.py
+++ tests/test_validador.py
@@ -4,9 +4,9 @@ from validador import validar_cpf
 class TestValidadorCPF(unittest.TestCase):
 
     def test_deve_retornar_true_para_cpf_valido(self):
-        cpf_com_mascara = "529.982.247-25" 
-        cpf_sem_mascara = "52998224725" 
-        cpf_com_espacos = " 529.982.247-25 "
+        cpf_com_mascara = "131.904.919-25" 
+        cpf_sem_mascara = "13190491925" 
+        cpf_com_espacos = " 131.904.919-25 "
 
         self.assertTrue(validar_cpf(cpf_com_mascara), "Deveria ser válido com máscara")
         self.assertTrue(validar_cpf(cpf_sem_mascara), "Deveria ser válido sem máscara")
@@ -15,7 +15,7 @@ class TestValidadorCPF(unittest.TestCase):
     def test_deve_rejeitar_entradas_invalidas_nulas_ou_vazias(self):
         self.assertFalse(validar_cpf(None), "Deveria rejeitar None (nulo)")
         self.assertFalse(validar_cpf(""), "Deveria rejeitar string vazia") #
-        self.assertFalse(validar_cpf("529.982.247-2X"), "Deveria rejeitar caractere inválido 'X'") #
+        self.assertFalse(validar_cpf("131.904.919-2X"), "Deveria rejeitar caractere inválido 'X'") #
 
     def test_deve_rejeitar_cpf_com_tamanho_invalido(self):
         self.assertFalse(validar_cpf("1234567890"), "Deveria rejeitar CPF com 10 dígitos") #
@@ -26,5 +26,5 @@ class TestValidadorCPF(unittest.TestCase):
         self.assertFalse(validar_cpf("11111111111"), "Deveria rejeitar sequência de 1s") #
 
     def test_deve_rejeitar_cpf_com_dv_incorreto(self):
-        self.assertFalse(validar_cpf("529.982.247-24"), "DV incorreto para CPF conhecido") #
+        self.assertFalse(validar_cpf("131.904.919-24"), "DV incorreto para CPF conhecido") #
         self.assertFalse(validar_cpf("123.456.789-00"), "DV incorreto para CPF genérico") #
