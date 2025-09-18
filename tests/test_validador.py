44f65374ce1530e66e58cafacabc1e601c547132
RenanCostaNunes <renandacostanunes@gmail.com>
2025-09-17 21:15:57 -0300

    Lógica do unitest corrigida

--- tests/test_validador.py
+++ tests/test_validador.py
@@ -1,15 +1,13 @@
+# tests/test_validador.py
 import unittest
-from cpf_validator.validador import validar_cpf
+# A linha abaixo é a correção crucial
+from validador import validar_cpf 
 
 class TestValidadorCPF(unittest.TestCase):
 
     def test_deve_retornar_true_para_cpf_valido(self):
-
         cpf_com_mascara = "529.982.247-25"
         cpf_sem_mascara = "52998224725"
 
         self.assertTrue(validar_cpf(cpf_com_mascara), "Deveria ser válido com máscara")
         self.assertTrue(validar_cpf(cpf_sem_mascara), "Deveria ser válido sem máscara")
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
